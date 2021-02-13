6e739715deb576357b52a3176ecf105b8b2100d3
ttngocthao <ttngocthao_87@yahoo.com>
2021-02-13 09:16:43 +0000

    added frontend build folder

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+app.use(express.static('build'))//point to frontend html file
 app.use(express.json())
 
 app.use(cors())
