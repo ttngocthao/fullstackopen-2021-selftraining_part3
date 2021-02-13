b6b8666a2f07b35b27ccd2b57b20a26c1fee5e4e
ttngocthao <ttngocthao_87@yahoo.com>
2021-02-13 06:32:14 +0000

    updated port

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post('/api/persons',(req,res)=>{
   return res.status(200).json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
